9b79c68502359c143a9ca47405dad2333ad7c53c
xcv58 <chenyihonglove@gmail.com>
2014-10-07 10:58:32 -0400

    disable color toggle.

--- toggle-display-leetcode.js
+++ toggle-display-leetcode.js
@@ -1,12 +1,12 @@
 (
     function()
     {
-        var color = $( ".notac" ).css("background-color");
-        if (color == "rgb(255, 255, 0)") {
-            $( ".notac" ).css("background-color", "");
-        } else {
-            $( ".notac" ).css("background-color", "yellow");
-        }
+        // var color = $( ".notac" ).css("background-color");
+        // if (color == "rgb(255, 255, 0)") {
+        //     $( ".notac" ).css("background-color", "");
+        // } else {
+        //     $( ".notac" ).css("background-color", "yellow");
+        // }
 
         var current = $( ".ac" ).parent().parent().css("display");
         if (current == "none") {
