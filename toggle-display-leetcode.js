c41e5084606805b4a551f6cd2b62304d58f3048d
xcv58 <chenyihonglove@gmail.com>
2015-07-20 18:48:52 -0400

    Update toggle-display-leetcode.js

--- toggle-display-leetcode.js
+++ toggle-display-leetcode.js
@@ -1,19 +1,6 @@
 (
     function()
     {
-        // var color = $( ".notac" ).css("background-color");
-        // if (color == "rgb(255, 255, 0)") {
-        //     $( ".notac" ).css("background-color", "");
-        // } else {
-        //     $( ".notac" ).css("background-color", "yellow");
-        // }
-
-        var current = $( ".ac" ).parent().parent().css("display");
-        if (current == "none") {
-            $( ".ac" ).parent().parent().css("display", "");
-        } else {
-            $( ".ac" ).parent().parent().css("display", "none");
-        }
-        return;
+        $('.ac').parent().parent().toggle();
     }
 )();
